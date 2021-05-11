756c90b8f754f2fef444e83476481c9ac4c01c97
Ali Azam <ali_1632@hotmail.co.uk>
2021-05-11 13:55:54 +0100

    Update token transfer

--- exercises/practice/token-transfer/.meta/reference/example-solution/token.sol
+++ exercises/practice/token-transfer/.meta/reference/example-solution/token.sol
@@ -1,9 +1,26 @@
 pragma solidity 0.6.12;
 
 contract Token {
-    function mint(uint32 tokens) external {}
+    mapping(address => uint256) balances;
 
-    function transfer(uint32 amount, address to) external {}
+    function mint(uint256 tokens) external {
+        require(tokens > 0, "cannot mint 0 tokens");
+        balances[msg.sender] += tokens;
+    }
 
-    function balanceOf(address addr) public view returns (uint32) {}
+    function transfer(uint256 value, address to) external {
+        // test that not transferring to zero address
+        require(to != address(0), "transfer to address 0 is not allowed");
+        // check that the value being sent is less than or equal to whats owned by msg.sender
+        require(value <= balances[msg.sender], "cannot send more than what you own");
+        // check for overflow
+        require(value + balances[to] >= balances[to], "overflow detected");
+
+        balances[msg.sender] -= value;
+        balances[to] += value;
+    }
+
+    function balanceOf(address addr) public view returns (uint256) {
+        return balances[addr];
+    }
 }
--- exercises/practice/token-transfer/contracts/token.sol
+++ exercises/practice/token-transfer/contracts/token.sol
@@ -1,26 +1,9 @@
 pragma solidity 0.6.12;
 
 contract Token {
-    mapping(address => uint256) balances;
+    function mint(uint32 tokens) external {}
 
-    function mint(uint256 tokens) external {
-        require(tokens > 0, "cannot mint 0 tokens");
-        balances[msg.sender] += tokens;
-    }
+    function transfer(uint32 amount, address to) external {}
 
-    function transfer(uint256 value, address to) external {
-        // test that not transferring to zero address
-        require(to != address(0), "transfer to address 0 is not allowed");
-        // check that the value being sent is less than or equal to whats owned by msg.sender
-        require(value <= balances[msg.sender], "cannot send more than what you own");
-        // check for overflow
-        require(value + balances[to] >= balances[to], "overflow detected");
-
-        balances[msg.sender] -= value;
-        balances[to] += value;
-    }
-
-    function balanceOf(address addr) public view returns (uint256) {
-        return balances[addr];
-    }
+    function balanceOf(address addr) public view returns (uint32) {}
 }
