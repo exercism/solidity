1666260a5c9945fd6e66638dedfbc66fca04b2cf
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-14 14:35:15 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible. (#43)

--- exercises/practice/hello-world/contracts/hello-world.sol
+++ exercises/practice/hello-world/contracts/hello-world.sol
@@ -2,6 +2,6 @@ pragma solidity 0.6.12;
 
 contract HelloWorld {
     function sayHelloWorld() external pure returns (string memory) {
-        return "Hello Universe";
+        return "Goodbye, Mars!";
     }
 }
